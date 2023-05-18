ea8c51aa07740ccc7b3a1cb971bed18f26d857c2
Joel Sanchez <joelalejandro.development@gmail.com>
2023-05-18 10:30:19 -0400

    eventos del juego añadidos

--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,82 @@
-let ancho, alto;
+const BotonJugar = document.getElementById("botonJugar");
+const BotonSelect = document.getElementById("selectButton");
+const SelectList = document.getElementById("selectOptions");
+const PantallaMenu = document.getElementById("menuScreen");
+const PantallaJuego = document.getElementById("gameScreen");
+const TableroJuego = document.getElementById("tablero") 
 let tablero
-let inicialX, inicialY
+let ancho, alto, minas
 
-function crearTablero(width, height)
+function crearTablero(ancho, alto, minas)
 {
+  // Crea el tablero con ancho y alto y lo rellena con ceros
   tablero = []
-  for (let i = 0; i < height; i++){
-    tablero.push(new Uint8Array(width))
+  tablero.length = alto
+  for (let i = 0; i < alto; i++)
+  {
+    tablero[i] = []
+    tablero[i].length = ancho
+    for (let j = 0; j < ancho; j ++)
+    {
+      tablero[i][j] = 0      
+    }
   }
-  for (let i = 0; i < height; i++){
-    for (let j = 0; j < width; j++){
-    tablero[i][j] = 0
+
+  let min = 0
+  // mientras sea menor al numero de minas
+  while (min < minas) {
+    let cx = parseInt(Math.random()*(ancho-1))
+    let cy = parseInt(Math.random()*(alto-1))
+    if (PosicionarMina(cx,cy)){
+      min++
     }
   }
 }
 
-function GenerarMinas(width, height) {
-  
-}
+// Ubica la mina en x,y; si existe una retorna false
 function PosicionarMina(x, y) {
   
   return true
 }
+
+// Detecta el inicio del juego
+
+BotonJugar.addEventListener("click", (e)=>{
+  switch (BotonSelect.dataset.dificultad) {
+    case "facil":
+      ancho = 9
+      alto = 9
+      minas = 10
+      break;
+      case "medio":
+        ancho = 16
+        alto = 16
+        minas = 40
+        break
+        case "dificil":
+          ancho = 30
+          alto = 16
+          minas = 99
+      break;
+  }
+  crearTablero(ancho, alto, minas)
+  
+  PantallaJuego.classList.remove("hidden")
+  PantallaJuego.classList.add("aparecer")
+  PantallaMenu.classList.add("disolver")
+  setTimeout(()=>{
+    PantallaMenu.classList.add("hidden")
+  },2000)
+})
+
+SelectList.addEventListener("click", (e)=>{
+  BotonSelect.dataset.dificultad = e.target.dataset.value
+  BotonSelect.textContent =  e.target.textContent
+})
+
+TableroJuego.addEventListener("click", (e)=>{
+  celda = e.target
+  if (celda.classList.contains("celda") && ! (celda.classList.contains("vista"))) {
+    celda.classList.add("vista")
+  }
+})
